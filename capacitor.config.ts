bf8d0144cd96dd2be0e53f16f9890f696cdb8a26
Phil0xFF <philmiletic@icloud.com>
2022-04-15 21:48:30 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'scroll-bug-demo',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
